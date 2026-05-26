4a06cee5fd776bc38b4433d668b6bd864f5541b3
Abinaya <abhinayakalimuthu@gmail.com>
2026-05-26 23:43:24 +0530

    deployment setup

--- hospital_system/settings.py
+++ hospital_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@xyfv50_j=5*!#pj98gf^0lk08vi2qabf7vbf9!h#zx4upa#%t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -186,6 +187,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
